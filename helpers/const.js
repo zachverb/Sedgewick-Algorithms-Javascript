bca24323b6dbc57b274f67f6bbd1de3600a733d9
zachverb <zachylax@gmail.com>
2015-09-18 18:11:43 -0700

    Added consts

---
+++ helpers/const.js
@@ -0,0 +1,4 @@
+import { All as sorts } from '../sorts';
+export const TEST_NUM = 100; // the number of times to generate tests
+export const TEST_SIZE = 1000; // the size of the test arrays
+export const SORTS = sorts;
